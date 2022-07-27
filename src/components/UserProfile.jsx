add51e01a508dca9ad047c4bb7ca7b0e78f3cb04
Utkarshini <utkarshinidubey24@gmail.com>
2022-07-28 00:14:57 +0530

    googleauthchanges

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { useParams, useNavigate } from 'react-router-dom';
-import { GoogleLogout } from 'react-google-login';
-
+// import { GoogleLogout } from 'react-google-login';
+import { googleLogout } from '@react-oauth/google';
 import { userCreatedPinsQuery, userQuery, userSavedPinsQuery } from '../utils/data';
 import { client } from '../client';
 import MasonryLayout from './MasonryLayout';
@@ -45,9 +45,10 @@ const UserProfile = () => {
   }, [text, userId]);
 
   const logout = () => {
-    localStorage.clear();
-
-    navigate('/login');
+    googleLogout().then(() => {
+      localStorage.clear();
+      navigate('/login');
+    });
   };
 
   if (!user) return <Spinner message="Loading profile" />;
@@ -73,21 +74,14 @@ const UserProfile = () => {
           </h1>
           <div className="absolute top-0 z-1 right-0 p-2">
             {userId === User.googleId && (
-              <GoogleLogout
-                clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
-                render={(renderProps) => (
-                  <button
-                    type="button"
-                    className=" bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
-                    onClick={renderProps.onClick}
-                    disabled={renderProps.disabled}
-                  >
-                    <AiOutlineLogout color="red" fontSize={21} />
-                  </button>
-                )}
-                onLogoutSuccess={logout}
-                cookiePolicy="single_host_origin"
-              />
+              <button
+                type="button"
+                className=" bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
+                onClick={logout}
+                // disabled={renderProps.disabled}
+              >
+                <AiOutlineLogout color="red" fontSize={21} />
+              </button>
             )}
           </div>
         </div>
